4f1c3cdc2d804ecc3bc8cf537e8b3c3d8a395612
Ilya Sotnikov <ilia12st@yandex.ru>
2026-05-05 22:19:06 +0300

    add gitsigns

--- lua/plugins/lspsaga-nvim.lua
+++ lua/plugins/lspsaga-nvim.lua
@@ -6,7 +6,12 @@ return {
 			move_in_saga = { prev = "<C-k>", next = "<C-j>" },
 			finder_action_keys = {
 				open = "<CR>",
-			},
+
+
+
+
+
+            },
 			definition_action_keys = {
 				edit = "<CR>",
 			},
